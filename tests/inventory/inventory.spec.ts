14274c0cd8844cb043254096deaf3bad861f6f91
Smolinski01 <smolinski001@gmail.com>
2026-04-21 23:26:59 +0200

    fix: correct invertory.spec.ts

--- tests/inventory/inventory.spec.ts
+++ tests/inventory/inventory.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from "@fixtures/base";
 
-test("should display inventory page after login", async ({ page }) => {
+test("should display inventory page after login", async ({ loggedInPage, page }) => {
   await expect(page).toHaveURL(/.*inventory.html/);
 });
 
-test("should display correct number of products", async ({ pages }) => {
-  await expect(pages.mainPage.classProduct).toHaveCount(0);
+test("should display correct number of products", async ({ loggedInPage, pages }) => {
+  await expect(pages.mainPage.classProduct).toHaveCount(6);
 });
 
-test("should add product to cart", async ({ loggedInPage, pages }) => {
-  await pages.mainPage.addItemToCart("Sauce Labs Backpack");
-  await expect(pages.mainPage.cartNumber).toHaveText("1");
+test("should add product to cart", async ({ loggedInPage }) => {
+  await loggedInPage.mainPage.addItemToCart("Sauce Labs Backpack");
+  await expect(loggedInPage.mainPage.cartNumber).toHaveText("1");
 });
